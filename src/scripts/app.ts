51516150c2a09437884ee261d866d43fe3728f19
Akbar Khaydarkhujayev <U0057@so.local>
2025-04-15 10:11:29 +0500

    load scripts

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,2 +1,10 @@
+import imgSrc from "../assets/image.png";
+
 const appDiv = document.getElementById("app")!;
+
 appDiv.innerHTML = "Hello from TypeScript!";
+
+const img = document.createElement("img");
+img.src = imgSrc;
+img.alt = "Example image";
+appDiv.appendChild(img);
